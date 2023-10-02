e2bd20300a1fa401fd2d62eb316417ff76b64d41
Mark Karels <mark.karels@ncino.com>
2023-10-02 09:36:25 -0400

    Updated UI

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,8 @@ from app.model import Roster, Game
 
 @app.route('/')
 def quiz():
+    start_scheduler()
     return render_template('index.html')
-
-
-@app.route('/start_scheduler', methods=['GET'])
-def start_scheduler():
-    scheduler.add_job(id='Fetch NFL Data Job', func=fetchdata.fetch_game_data(), trigger='interval', minutes=5)
-    return "Scheduler started", 200
 
 
 @app.route('/generate_question', methods=['POST'])
@@ -21,15 +16,13 @@ def generate_question_route():
     selected_team = data.get("team", "Chicago Bears")
     team_alias = fetchdata.get_team_alias(selected_team)
 
-    # Loop through all weeks to check if data exists
     for week in range(1, 19):
-        # Check if roster data exists for the selected team for the given week
         roster_exists = Roster.query.filter_by(team=team_alias).first()
         if not roster_exists:
             fetchdata.fetch_roster_data(team_alias)
 
-        # Check if game data exists for the selected team for the given week
-        game_exists = Game.query.filter((Game.home_team == team_alias) | (Game.away_team == team_alias)).filter_by(week_num=week).first()
+        game_exists = (Game.query.filter((Game.home_team == team_alias) | (Game.away_team == team_alias)).filter_by
+                       (week_num=week).first())
         if not game_exists:
             try:
                 fetchdata.fetch_game_data(week, team_alias)
@@ -48,3 +41,9 @@ def generate_question_route():
         "options": options,
         "correct_option": correct_option
     })
+
+
+def start_scheduler():
+    scheduler.add_job(id='Fetch NFL Data Job', func=fetchdata.fetch_game_data, args=[4, 'Chicago Bears'],
+                      trigger='interval', minutes=5)
+    return "Scheduler started", 200
